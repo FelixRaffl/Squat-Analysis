d90a20203cea1e5e1ef9fb80b6b1da6e44c482fa
FelixRaffl <felix.raffl7@gmail.com>
2024-10-10 13:00:07 +0200

    detection works but only with phone

--- Project/main.py
+++ Project/main.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from matplotlib.figure import Figure 
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk)
-
 laptopcam = cv2.VideoCapture(0) #opens the default camera
 if not laptopcam.isOpened():
     print("Cannot open camera")
@@ -11,11 +10,22 @@ else:
     print("A camera was opened")
 
 ret, frame = laptopcam.read()
-
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-parameters = cv2.aruco.DetectorParameters()
-detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+cv2.imshow('Detected Markers', frame)
+cv2.waitKey(0)
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+parameters = cv2.aruco.DetectorParameters()
 
+# Create the ArUco detector
+
+detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+# Detect the markers
 corners, ids, rejected = detector.detectMarkers(gray)
+# Print the detected markers
+print("Detected markers:", ids)
+if ids is not None:
+    cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+    cv2.imshow('Detected Markers', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
